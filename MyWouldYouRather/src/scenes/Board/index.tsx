d437ab69ae55512e427724601782d9227d3a3c80
Juan Manuel Oliú <juan@xmartlabs.com>
2020-07-26 18:55:11 -0300

    - app min structure

--- MyWouldYouRather/src/scenes/Board/index.tsx
+++ MyWouldYouRather/src/scenes/Board/index.tsx
@@ -8,27 +8,20 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React from 'react'
+import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native'
 
 import {
   Header,
   LearnMoreLinks,
-  Colors,
   DebugInstructions,
   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+} from 'react-native/Libraries/NewAppScreen'
+import { styles } from './styles'
 
-declare const global: {HermesInternal: null | {}};
+declare const global: { HermesInternal: null | {} }
 
-const App = () => {
+export const Board = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -73,46 +66,5 @@ const App = () => {
         </ScrollView>
       </SafeAreaView>
     </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  )
+}
